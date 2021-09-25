ddeb4756706261e973b879af7bb0cb26ff70d3cd
Silas-py <2669128599@qq.com>
2021-09-25 08:08:35 +0800

    第四章

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,60 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@SCREEN
+D=A 		
+@1
+M=D 		// 地址 1 储存 SCREEN 的地址
+(CHECK)
+
+@KBD
+D=M 		// 加载 RAM[24576] 的值
+@BLACK
+D;JNE 		// if D > 0 goto BLACK
+
+@WHITE
+D;JEQ 		// if D = 0 goto WHITE
+
+(BLACK)
+@1
+D=A 		// D = 屏幕指针的地址
+@24575
+D=D-A 		// D = i - 24575(最后一个像素点位置)
+@END
+D;JGT 		// if (i-24575) > 0 goto END
+
+@1
+D=M 		// 把地址1表示的屏幕地址储存到 D 寄存器中
+A=M 		// 当前地址推入A中
+M=-1 		// -1的补码为11111111111111111111，即16位全部为黑的
+
+@1
+M=D+1 		// 当前地址+1
+@BLACK
+0;JMP
+
+(WHITE)
+@1
+D=A 		// D = 屏幕指针的地址
+@SCREEN
+D=M
+@1
+D=D-M 		// D = i - SCREEN(最后一个像素点位置)
+@END
+D;JLT 		// if (i-24575) < 0 goto END
+
+@1
+D=M 		// 缓存当前地址
+A=M 		// 当前地址推入A中
+M=0 		// 地址对应部分，清空
+
+@1
+M=D-1 		// 当前地址-1
+@WHITE
+0;JMP
+
+(END)
+
+@CHECK
+0;JMP 		// 无限循环
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+//主要参考书上从1到100的累加，修改而来
+
+@2
+M=0 		// R2 = 0, 先初始化 R2，不然前两个测试过不去
+@i
+M=1 		// i = 1
+@sum
+M=0  		// sum = 0
+(LOOP)
+@i
+D=M 		// D = i
+@1
+D=D-M 		// D = i - R1
+@END
+D;JGT 		// if (i-R1) > 0 goto END
+@0
+D=M 		// D = R0
+@sum
+M=D+M 		// sum = sum + R0
+@sum
+D=M 		// D = sum
+@2
+M=D 		// R2 = D
+@i
+M=M+1 		// i = i + 1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
