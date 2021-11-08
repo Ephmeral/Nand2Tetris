5da95e2cc5e1e026339e2eb8400403e3c3a8ba7e
Silas-py <2669128599@qq.com>
2021-11-08 13:28:23 +0800

    修改README文件

---
+++ projects/07/src/CommandType.java
@@ -0,0 +1,11 @@
+interface CommandType {
+    String C_ARITHMETIC = "1"; //
+    String C_PUSH = "2"; //
+    String C_POP = "3"; //
+    String C_LABEL = "4"; //
+    String C_GOTO = "5"; //
+    String C_IF = "6"; //
+    String C_FUNCTION = "7"; //
+    String C_RETURN = "8"; //
+    String C_CALL = "9"; //
+}
